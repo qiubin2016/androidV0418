1aabdd70849e1c1ccf19d7da16caf2d27da69cbd
crazy <qiubin2018@gmail.com>
2019-12-03 13:36:02 +0800

    1、增加存储权限申请； 2、增加注释；

--- app/src/main/java/com/huawei/agentlitedemo/activity/AgentLiteMain.java
+++ app/src/main/java/com/huawei/agentlitedemo/activity/AgentLiteMain.java
@@ -1,9 +1,15 @@
 package com.huawei.agentlitedemo.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -27,6 +33,16 @@ public class AgentLiteMain extends BaseAty {
     private static final String TAG = "AgentLiteMain";
     private static String configFile = "config.properties";
 
+    //读写权限
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    //请求状态码
+    private static int REQUEST_PERMISSION_CODE = 1;
+
+    private int count = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +53,20 @@ public class AgentLiteMain extends BaseAty {
         try {
             Context context = getApplicationContext();
 
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+                }
+            }
+
+            //获取sdcard路径
+            File sdcardPath = Environment.getExternalStorageDirectory();
+            Log.i(TAG, "sdcardPath = " + sdcardPath.toString());
+            //sdcardPath = /storage/emulated/0
+
             String workPath = FileUtil.getWorkDir(context).getAbsolutePath() + "/AgentLiteDemo";
+            //默认workPath无权限进入，修改路径
+            workPath = "/sdcard" + "/AgentLiteDemo";
             String logPath = workPath + "/log";
             File dir = new File(logPath);
             dir.mkdirs();
@@ -47,6 +76,7 @@ public class AgentLiteMain extends BaseAty {
 
             FileUtil.copyAssetDirToFiles(context, "conf");
 
+            //  /sdcard/AgentLiteDemo/conf/config.properties
             loadProperties(workPath + "/conf/" + configFile);  //读取配置文件
             loadSharedData();  //读取数据库数据
 
@@ -59,6 +89,13 @@ public class AgentLiteMain extends BaseAty {
             //Throwable有两个重要的子类：Exception（异常）和 Error（错误），二者都是 Java 异常处理的重要子类，各自都包含大量子类
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        count++;
+        Log.i(TAG, "onResume++++++++++++++++++++++:" + count);
     }
 
     @Override
@@ -156,6 +193,7 @@ public class AgentLiteMain extends BaseAty {
             if (AgentLiteUtil.hasInitialized()) {
                 return;
             }
+            // /sdcard/AgentLiteDemo/conf/config.properties
             File configfile = new File(configFile);
             /*加载调测的配置文件*/
             if (configfile.exists()) {
@@ -166,6 +204,16 @@ public class AgentLiteMain extends BaseAty {
             }
         } catch (Throwable t) {
             Log.e(TAG, "setSystemInfo error.reason:{0}.", t);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            for (int i = 0; i < permissions.length; i++) {
+                Log.i("MainActivity", "申请的权限为：" + permissions[i] + ",申请结果：" + grantResults[i]);
+            }
         }
     }
 }
